20df2261d470d141dbac09f51d98b616058df073
kato-seigou <seigou444@gmail.com>
2026-02-05 18:51:07 +0900

    feat: 各テーブルの定義を実装

--- app/sql_app/models.py
+++ app/sql_app/models.py
@@ -1,13 +1,56 @@
 # データの構造を書く
-from datetime import datetime
-from sqlalchemy import (Column, ForeignKey, Integer, String, DateTime)
+from datetime import datetime, timezone
+from sqlalchemy import (Column, ForeignKey, Integer, String, DateTime, Date)
 from .database import Base
 
 class User(Base):
     __tablename__ = "users"
     user_id = Column(Integer, primary_key=True, index=True)
-    login_id = Column(String(10), nullable=False, unique=True)
+    login_id = Column(String(10), nullable=False, unique=True, index=True)
     display_name = Column(String(12), nullable=True, unique=False, index=True)
     password_hash = Column(String, nullable=False, unique=False)
-    created_at = Column(DateTime, nullable=False, unique=False, default=datetime.utcnow)
+    created_at = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        default=lambda: datetime.now(timezone.utc)
+    )
+    
+class Discography(Base):
+    __tablename__ = "discographies"
+    discography_id = Column(Integer, primary_key=True)
+    discography_title = Column(String, nullable=False, unique=True, index=True)
+    discography_num = Column(Integer, nullable=False, unique=False)
+    discography_type = Column(String, nullable=False, unique=False)
+    released_date = Column(Date, nullable=False, unique=False)
+    playtime_seconds = Column(Integer, nullable=False, unique=False)
+    created_at = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        default=lambda: datetime.now(timezone.utc)
+    )
+    
+class ListenLog(Base):
+    __tablename__ = "listen_logs"
+    log_id = Column(Integer, primary_key=True)
+    user_id = Column(
+        Integer, 
+        ForeignKey("users.user_id", ondelete="CASCADE"), 
+        nullable=False
+                     )
+    discography_id = Column(
+        Integer, 
+        ForeignKey("discographies.discography_id", ondelete="RESTRICT"), 
+        nullable=False
+        )
+    listened_at = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        default=lambda: datetime.now(timezone.utc)
+    )
+    created_at = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        default=lambda: datetime.now(timezone.utc)
+    )
+    deleted_at = Column(DateTime(timezone=True), nullable=True) 
     
