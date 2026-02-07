9561d165bf07cd8cda6ecb1ac0a78dda15f804cd
kato-seigou <seigou444@gmail.com>
2026-02-07 13:24:21 +0900

    feat: schemas.pyのデータ構造を定義

---
+++ app/sql_app/schemas.py
@@ -0,0 +1,64 @@
+import datetime
+from enum import Enum
+from pydantic import BaseModel, Field, ConfigDict
+from typing import Optional
+
+# FastAPI側のデータ構造を定義する
+
+### user関連
+class UserBase(BaseModel):
+    # UserCreateをUserReadで継承するとresに
+    # passwordが含まれてしまう
+    display_name: Optional[str] = Field(default=None, max_length=12)
+    login_id: str = Field(max_length=10)
+    
+class UserCreate(UserBase):
+    password: str = Field(min_length=8, max_length=12)
+    
+class UserRead(UserBase):
+    # 表示用
+    user_id: int
+    created_at: datetime.datetime
+    
+    # pydantic: v2.8.2
+    model_config = ConfigDict(from_attributes=True)
+    
+### discography関連
+class DiscographyType(str, Enum):
+    Album = "Album"
+    EP = "EP"
+    Single = "Single"
+    Movie = "Movie"
+    
+class DiscographyCreate(BaseModel):
+    discography_title: str = Field(min_length=1)
+    discography_num: int = Field(min_length=1) # ディスコグラフィの収録曲数
+    discography_type: DiscographyType
+    released_date: datetime.date
+    playtime_seconds: int = Field(ge=1)
+
+class DiscographyRead(DiscographyCreate):
+    discography_id: int
+    created_at: datetime.datetime
+
+    model_config = ConfigDict(from_attributes=True)
+    
+
+### ログ関連
+class ListenLogCreate(BaseModel):
+    user_id: int
+    discography_id: int
+    comment: Optional[str] = Field(default=None, max_length=50)
+    listened_at: datetime.datetime
+    
+class ListenLogUpdate(BaseModel):
+    discography_id: Optional[int]
+    comment: Optional[str] = Field(default=None, max_length=50)
+    listened_at: Optional[datetime.datetime] = None
+    
+class ListenLogRead(ListenLogCreate):
+    log_id: int
+    created_at: datetime.datetime
+    deleted_at: Optional[datetime.datetime]
+    
+    model_config = ConfigDict(from_attributes=True)
