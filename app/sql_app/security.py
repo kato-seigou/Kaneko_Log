84794800df23948958a70272c91fa00a651dcb81
kato-seigou <seigou444@gmail.com>
2026-02-08 12:59:42 +0900

    feat: 管理者チェック用の関数require_adminの作成絵

--- app/sql_app/security.py
+++ app/sql_app/security.py
@@ -108,3 +108,12 @@ def get_current_user(
     if user is None:
         raise credentials_exception
     return user
+
+# 管理者チェック用のDependency
+def require_admin(current_user: models.User = Depends(get_current_user)) -> models.User:
+    if not current_user.is_admin:
+        raise HTTPException(
+            status_code = status.HTTP_403_FORBIDDEN,
+            detail = "Admin privileges required"
+        )
+    return current_user
