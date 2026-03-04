ca1fbaebe69126ced0b85fda5805f179a7683437
kato-seigou <seigou444@gmail.com>
2026-03-04 15:25:49 +0900

    fix: 【main.py】 bind=engine

--- app/app/main.py
+++ app/app/main.py
@@ -29,7 +29,7 @@ app = FastAPI(title="kanekoayano App API")
 # Base.metadata.create_all(bind=engine)
 @app.on_event("startup")
 def create_tables():
-    Base.metadata.create_all(bind="engine")
+    Base.metadata.create_all(bind=engine)
 
 # css
 BASE_DIR = Path(__file__).resolve().parent
