e95b6ec1ba9422661092a4b7546574189e7d4980
kato-seigou <seigou444@gmail.com>
2026-03-05 18:15:03 +0900

    fix: 【xmlファイル】 サイトマップに直接アクセスできるようにした

--- app/app/main.py
+++ app/app/main.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import RedirectResponse, HTMLResponse
+from fastapi.responses import RedirectResponse, HTMLResponse, FileResponse
 from fastapi.exceptions import HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -37,6 +37,11 @@ STATIC_DIR = BASE_DIR / "static"
 TEMPLATES_DIR = BASE_DIR / "templates"
 app.mount("/static", StaticFiles(directory=str(STATIC_DIR)), name="static")
 templates = Jinja2Templates(directory=str(TEMPLATES_DIR))
+
+# サイトマップ
+@app.get("/sitemap.xml")
+def sitemap():
+    return FileResponse("app/static/sitemap.xml")
 
 # app.add_middleware(
 #     CORSMiddleware,
